fb8cc106c7b3fce6f2585a73bfb0ac9ce9aaa23d
Dozed12 <ftmmsm@sapo.pt>
2018-01-08 21:37:20 +0000

    Small Ident fix

--- library/p5.voronoi.js
+++ library/p5.voronoi.js
@@ -103,18 +103,18 @@ const VOR_CELLDRAW_SITE = 3;
 	function insidePoly(point, poly){
 
 		var x = point[0], y = point[1];
-	    
-	    var inside = false;
-	    for (var i = 0, j = poly.length - 1; i < poly.length; j = i++) {
-	        var xi = poly[i][0], yi = poly[i][1];
-	        var xj = poly[j][0], yj = poly[j][1];
-	        
-	        var intersect = ((yi > y) != (yj > y)) && (x < (xj - xi) * (y - yi) / (yj - yi) + xi);
-	        if (intersect)
-	        	inside = !inside;
-	    }
-	    
-	    return inside;
+		
+		var inside = false;
+		for (var i = 0, j = poly.length - 1; i < poly.length; j = i++) {
+			var xi = poly[i][0], yi = poly[i][1];
+			var xj = poly[j][0], yj = poly[j][1];
+			
+			var intersect = ((yi > y) != (yj > y)) && (x < (xj - xi) * (y - yi) / (yj - yi) + xi);
+			if (intersect)
+				inside = !inside;
+		}
+		
+		return inside;
 	}
 
 	//Render
